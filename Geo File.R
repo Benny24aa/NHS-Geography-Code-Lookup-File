0071f4c10b4ca70833c0a07f83d86d7916120e40
Benny24aa <harleyb101010@gmail.com>
2025-02-09 11:35:38 +0000

    libraries and resources called upon using phsopendata library, and minor changes to hb lookup (data cleaning)

---
+++ Geo File.R
@@ -0,0 +1,44 @@
+library(shiny)
+library(shinyWidgets)
+library(DT)
+library(data.table)
+library(ggplot2)
+library(shinydashboard)
+library(readr)
+library(dplyr)
+library("readxl")
+library("RColorBrewer")
+library(rsconnect)
+library(base)
+library(datasets)
+library(graphics)
+library(grDevices)
+library(methods)
+library(readr)
+library(readxl)
+library(RColorBrewer)
+library(stats)
+library(utils)
+library(sf)
+library(writexl)
+library(base64enc)
+library(phsopendata)
+library(plotly)
+library(crosstalk)
+library(lubridate)
+library(kableExtra)
+library(tidyverse)
+
+
+HB_Lookup <- get_resource(res_id = "652ff726-e676-4a20-abda-435b98dd7bdc")
+Council_Lookup <- get_resource(res_id = "967937c4-8d67-4f39-974f-fd58c4acfda5")
+Interminate_Zone_Lookup <- get_resource(res_id = "e3e885cc-2530-4b3c-bead-9eda9782264f")
+Data_Zone_Lookup  <- get_resource(res_id = "395476ab-0720-4740-be07-ff4467141352")
+Hospital_Lookup <- get_resource(res_id = "c698f450-eeed-41a0-88f7-c1e40a568acc")
+
+HB_Lookup <- HB_Lookup |>
+  select(-Country,-HBDateEnacted)|>
+  filter(is.na(HBDateArchived))|>
+  select(-HBDateArchived) %>% 
+  mutate(GeoType = "Health Board")
+
